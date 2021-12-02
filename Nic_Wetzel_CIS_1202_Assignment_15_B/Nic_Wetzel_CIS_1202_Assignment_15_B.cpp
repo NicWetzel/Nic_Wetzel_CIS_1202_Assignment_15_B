4d150df5593427b15142e54b14b170888e462f32
Nicholas Wetzel <tattoosbynic@gmail.com>
2021-12-02 17:19:39 -0500

    Add project files.

---
+++ Nic_Wetzel_CIS_1202_Assignment_15_B/Nic_Wetzel_CIS_1202_Assignment_15_B.cpp
@@ -0,0 +1,26 @@
+// Nic Wetzel
+// CIS 1202 501
+// December 2, 2021
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+
+T half(T value)
+{
+
+}
+
+
+int main()
+{
+    int a = 7.0;
+    float b = 5.0f;
+    double c = 3;
+
+    cout << "Half value of " << a << ": " << half(a) << endl;
+    cout << "Half value of " << b << ": " << half(b) << endl;
+    cout << "Half value of " << c << ": " << half(c) << endl;
+}
