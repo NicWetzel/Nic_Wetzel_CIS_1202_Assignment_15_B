760bfc100ef746b37cf8c00ce4b38e15d2cfe540
Nicholas Wetzel <tattoosbynic@gmail.com>
2021-12-02 22:34:07 -0500

    Added coments to describe what is happening in the template as well as the main function. Still need to add the contents of the template function but unsure of how to accomplish at the moment.

--- Nic_Wetzel_CIS_1202_Assignment_15_B/Nic_Wetzel_CIS_1202_Assignment_15_B.cpp
+++ Nic_Wetzel_CIS_1202_Assignment_15_B/Nic_Wetzel_CIS_1202_Assignment_15_B.cpp
@@ -5,9 +5,10 @@
 #include <iostream>
 
 using namespace std;
-
+// Defining template to use the half function
 template <typename T>
 
+// Function to find half of each value passed
 T half(T value)
 {
 
@@ -16,10 +17,12 @@ T half(T value)
 
 int main()
 {
+    // Variables to send into the half function template
     int a = 7.0;
     float b = 5.0f;
     double c = 3;
 
+    // Cout statements that show the number and what half of that number is
     cout << "Half value of " << a << ": " << half(a) << endl;
     cout << "Half value of " << b << ": " << half(b) << endl;
     cout << "Half value of " << c << ": " << half(c) << endl;
